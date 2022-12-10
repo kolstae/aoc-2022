6facaa9ac30a1e45243c8969b624018cac61d2df
Espen Amble Kolstad <espen.kolstad@ardoq.com>
2022-12-10 10:02:26 +0100

    Day 10

---
+++ src/day10.clj
@@ -0,0 +1,35 @@
+(ns day10
+  (:require
+   [clojure.string :as str]))
+
+(def input (slurp (str "resources/" *ns* ".txt")))
+(def small-input "addx 15\naddx -11\naddx 6\naddx -3\naddx 5\naddx -1\naddx -8\naddx 13\naddx 4\nnoop\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx -35\naddx 1\naddx 24\naddx -19\naddx 1\naddx 16\naddx -11\nnoop\nnoop\naddx 21\naddx -15\nnoop\nnoop\naddx -3\naddx 9\naddx 1\naddx -3\naddx 8\naddx 1\naddx 5\nnoop\nnoop\nnoop\nnoop\nnoop\naddx -36\nnoop\naddx 1\naddx 7\nnoop\nnoop\nnoop\naddx 2\naddx 6\nnoop\nnoop\nnoop\nnoop\nnoop\naddx 1\nnoop\nnoop\naddx 7\naddx 1\nnoop\naddx -13\naddx 13\naddx 7\nnoop\naddx 1\naddx -33\nnoop\nnoop\nnoop\naddx 2\nnoop\nnoop\nnoop\naddx 8\nnoop\naddx -1\naddx 2\naddx 1\nnoop\naddx 17\naddx -9\naddx 1\naddx 1\naddx -3\naddx 11\nnoop\nnoop\naddx 1\nnoop\naddx 1\nnoop\nnoop\naddx -13\naddx -19\naddx 1\naddx 3\naddx 26\naddx -30\naddx 12\naddx -1\naddx 3\naddx 1\nnoop\nnoop\nnoop\naddx -9\naddx 18\naddx 1\naddx 2\nnoop\nnoop\naddx 9\nnoop\nnoop\nnoop\naddx -1\naddx 2\naddx -37\naddx 1\naddx 3\nnoop\naddx 15\naddx -21\naddx 22\naddx -6\naddx 1\nnoop\naddx 2\naddx 1\nnoop\naddx -10\nnoop\nnoop\naddx 20\naddx 1\naddx 2\naddx 2\naddx -6\naddx -11\nnoop\nnoop\nnoop\n")
+
+(defn op->deltas [s]
+  (cond-> [0]
+    (str/starts-with? s "addx") (conj (parse-long (subs s (count "addx "))))))
+
+(comment
+
+ (let [_input small-input]
+   (->> (str/split-lines input)
+        (mapcat op->deltas)
+        (reductions + 1)
+        (map-indexed #(vector (inc %1) %2))
+        (drop 19)
+        (take-nth 40)
+        (map (partial apply *))
+        (apply +)))
+ ;; part-1 14560
+
+ (let [_input small-input]
+   (->> (str/split-lines input)
+        (mapcat op->deltas)
+        (reductions + 1)
+        (map-indexed (fn [i x]
+                       (if (<= (dec x) (mod i 40) (inc x)) \# \space)))
+        (partition 40)
+        (map str/join)
+        ))
+ ;; part-2 EKRHEPUZ
+ )
